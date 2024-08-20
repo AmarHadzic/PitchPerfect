2d80724d6c2b2c7bc5cba759e3a8566fb74a9409
AmarHadzic <amar.hadzic@slu.edu>
2024-08-20 03:57:33 -0500

    header complete

--- pitch-perfect/src/app/app.component.ts
+++ pitch-perfect/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LiveScoresComponent } from './live-scores/live-scores.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LiveScoresComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, AngularMaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
