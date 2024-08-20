1cc35996139fd78752213a451a7afdcabb015249
Adem Durakovic <adem.durakovic@slu.edu>
2024-08-20 15:59:48 -0500

    out heee service

--- pitch-perfect/src/app/league-table/league-table.component.ts
+++ pitch-perfect/src/app/league-table/league-table.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LeagueApiServiceService } from '../league-api-service.service';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+
 
 @Component({
   selector: 'app-league-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './league-table.component.html',
-  styleUrl: './league-table.component.scss'
+  styleUrls: ['./league-table.component.scss'] 
 })
-export class LeagueTableComponent {
+export class LeagueTableComponent implements OnInit {
 
+  message: string | undefined;
+
+  constructor(private league_service: LeagueApiServiceService) {}
+
+  ngOnInit(): void {
+    this.league_service.get_message("hello from adem").subscribe((data) => {
+      this.message = data;
+    });
+  }
 }
